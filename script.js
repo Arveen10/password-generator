431ff77375663cd4c43f19f4eab4fff825f8c8b1
Arveen <arveensuresh22@gmail.com>
2024-02-15 21:43:09 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('generateBtn').addEventListener('click', function (){
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let passwordLength = 12;
+    let password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    document.getElementById("password").value = password;
+});
